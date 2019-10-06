bad2fe47c20f4d3d2db738344a276a2349550f82
Alex Ghiban <drew7721@gmail.com>
2019-10-06 08:53:04 -0400

    Create separate config tab for visibility in production mode.

--- Plugin/WrapperInterface.php
+++ Plugin/WrapperInterface.php
@@ -8,6 +8,6 @@ namespace JustinKase\LayoutHints\Plugin;
  */
 interface WrapperInterface
 {
-    const JK_CONFIG_BLOCK_HINTS_STATUS = 'dev/justinkase_hints/justinkase_hints_status';
+    const JK_CONFIG_BLOCK_HINTS_STATUS = 'justinkase_hints/global_setting/status';
     const JK_TEMPLATE = '<div class="justinkase-hint"><code><strong>%s</strong> %s <span class="justinkase-hint-classname">%s</span></code>%s</div>';
 }
