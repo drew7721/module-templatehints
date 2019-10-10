3d5f92f276553cb3bcda3410cf62175ad5b801bd
Alex Ghiban <drew7721@gmail.com>
2019-10-10 11:26:00 -0400

    Write js code to toggle the body class.

--- view/frontend/web/js/jk-hints.js
+++ view/frontend/web/js/jk-hints.js
@@ -1,3 +1,12 @@
 /**
- * Write code to enable/disable hints from js keypress.
+ * Set a listener for keys `Shift + Ctrl + H` to trigger the display of the hints on the page.
+ *
+ * @author Alex Ghiban <drew7721@gmail.com>
  **/
+(function(){
+    document.addEventListener('keydown', function (e) {
+        if (e.key === 'H' && e.shiftKey && e.ctrlKey) {
+            document.getElementsByTagName('body')[0].classList.toggle('justinkase-hints-enabled');
+        }
+    })
+}());
